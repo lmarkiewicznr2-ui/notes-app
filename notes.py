95464348a8492fe89339cc9bd34bc687ca373afa
Leon Marki <lmarkiewicznr2@gmail.com>
2026-04-12 17:24:56 +0200

    B: Implemented add note writing to .txt file

--- notes.py
+++ notes.py
@@ -6,6 +6,9 @@ command = sys.argv[1]
 
 if command == "add":
     print("adding note")
+    note = sys.argv[2]
+    with open("notes.txt", "a") as file:
+        file.write(note + "\n")
 elif command == "list":
     print("listing notes")
 else:
