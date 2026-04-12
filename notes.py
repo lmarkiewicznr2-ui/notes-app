f813ef5b63b09a34ef8fd7b46f9da76b9e9e0e7c
Leon Marki <lmarkiewicznr2@gmail.com>
2026-04-12 17:57:14 +0200

    G: Biggest commit yet, introduced numbering on the notes and fix for error when no notes found.~

--- notes.py
+++ notes.py
@@ -1,18 +1,33 @@
 import sys
-
+usage = "Usage:\n  python notes.py add <note>\n  python notes.py list"
+if len(sys.argv) < 2:
+    print(usage)
+    sys.exit()
 
 command = sys.argv[1]
 
 
 if command == "add":
-    print("adding note")
+    if len(sys.argv) < 3:
+        print(usage)
+        sys.exit()
     note = sys.argv[2]
-    with open("notes.txt", "a") as file:
-        file.write(note + "\n")
+    try:
+        with open("notes.txt", "r") as file:
+            lines = file.read().splitlines()
+    except FileNotFoundError:
+        lines = []
+    lines.append(note)
+    with open ("notes.txt", "w") as file:
+        for i, n in enumerate(lines, start=1):
+            file.write(f"{i}. {n}\n")
 elif command == "list":
-    print("listing notes")
-    with open("notes.txt", "r") as file:
-        notes = file.read()
-        print(notes)
+    
+    try:
+        with open("notes.txt", "r") as file:
+            print(file.read())
+    except FileNotFoundError:
+        print("No notes yet")
+
 else:
-    print("unknown command")
+    print("Unknown command. Use: add or list")
