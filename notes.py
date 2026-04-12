8f4c661d30f51dfd59f2f448e7d34f63cafd7ebc
Leon Marki <lmarkiewicznr2@gmail.com>
2026-04-12 20:01:56 +0200

    J: Simplified the code into functions, extended the usage and the unknown command

--- notes.py
+++ notes.py
@@ -1,53 +1,85 @@
 import sys
-usage = "Usage:\n  python notes.py add <note>\n  python notes.py list"
+
+
+
+usage = """
+Usage:
+  python notes.py add <note>
+  python notes.py list
+  python notes.py delete <number>
+"""
+
+
+
+
+
+
+
 if len(sys.argv) < 2:
     print(usage)
     sys.exit()
 
 command = sys.argv[1]
 
-
-if command == "add":
+def add_note():
     if len(sys.argv) < 3:
         print(usage)
         sys.exit()
+
     note = " ".join(sys.argv[2:])
+
     try:
         with open("notes.txt", "r") as file:
-            lines = file.read().splitlines()
+            lines = [line.split(". ", 1)[1] for line in file.read().splitlines()]
     except FileNotFoundError:
         lines = []
+
     lines.append(note)
-    with open ("notes.txt", "w") as file:
+
+    with open("notes.txt", "w") as file:
         for i, n in enumerate(lines, start=1):
             file.write(f"{i}. {n}\n")
-elif command == "list":
-    
+def list_notes():
     try:
         with open("notes.txt", "r") as file:
             print(file.read())
     except FileNotFoundError:
         print("No notes yet!")
-elif command == "delete":
+
+def delete_note():
     if len(sys.argv) < 3:
         print("Usage: python notes.py delete <number>")
         sys.exit()
 
     try:
         with open("notes.txt", "r") as file:
-            index = int(sys.argv[2])
             lines = file.read().splitlines()
-            if index < 1 or index > len(lines): 
-                print("Invalid note number")
-                sys.exit()
-            lines.pop(index - 1)
-            with open("notes.txt","w") as file:
-                for i, n in enumerate(lines, start=1):
-                    file.write(f"{i}. {n}\n")
+
+        index = int(sys.argv[2])
+
+        if index < 1 or index > len(lines):
+            print("Invalid note number")
+            sys.exit()
+
+        lines.pop(index - 1)
+
+        with open("notes.txt", "w") as file:
+            for i, n in enumerate(lines, start=1):
+                file.write(f"{i}. {n}\n")
+
     except FileNotFoundError:
         print("No notes yet!")
         sys.exit()
 
 
+
+
+
+if command == "add":
+    add_note()
+elif command == "list":
+    list_notes()
+elif command == "delete":
+    delete_note()
 else:
-    print("Unknown command. Use: add or list")
+    print("Unknown command. Use: add, list, delete")
